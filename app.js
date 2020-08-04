cee32548a5a9de07b0f2d95a00da97e5ac01d88f
srivardhan66 <69209359+srivardhan66@users.noreply.github.com>
2020-08-04 21:23:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+var fconst firebaseConfig = {
+  apiKey: "AIzaSyA3yutoVi2Or_RxO0RueXhNMHUCCE4C8VQ",
+  authDomain: "phishing-ff75f.firebaseapp.com",
+  databaseURL: "https://phishing-ff75f.firebaseio.com",
+  projectId: "phishing-ff75f",
+  storageBucket: "phishing-ff75f.appspot.com",
+  messagingSenderId: "230812025916",
+  appId: "1:230812025916:web:ed9c0683d7dfe0ab9eef39",
+  measurementId: "G-EZQ30NLFM4"
 };
-
      
  
 ////Do Not Edit Below This
